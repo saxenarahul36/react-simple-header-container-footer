673122fd41f8ac73821c084e1c02233658b5a0b4
Rahul Saxena <saxenarahul36@gmail.com>
2022-04-04 23:51:22 +0800

    Added background images animation slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./common/header";
 import Footer from "./common/footer";
-import styled from "styled-components";
-
-const AppContainer = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
+import ImageSlider from "./common/ImageSlider";
 
 const App = () => {
   return (
-    <AppContainer>
+    <div className="page-container">
       <Header />
+      <ImageSlider />
       <main>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
           <Redirect to="/"></Redirect>
         </Switch>
       </main>
       <Footer />
-    </AppContainer>
+    </div>
   );
 };
 
--- src/common/navBar/ui.jsx
+++ src/common/navBar/ui.jsx
@@ -4,6 +4,37 @@ export const NavBarWreper = styled.div`
   ul {
     padding: 0px;
   }
+  ul li {
+    float: left;
+    background-color: gray;
+    list-style: none;
+    color: white;
+    width: 120px;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    opacity: 1;
+    cursor: pointer;
+    :hover {
+      ul {
+        visibility: visible;
+        opacity: 1;
+        z-index: 1;
+        transform: translateY(0%);
+        transition-delay: 0s, 0s, 0.3s;
+      }
+    }
+  }
+  ul li ul li {
+    :hover {
+      a {
+        padding-left: 10px;
+        // color: blue;
+        font-weight: bold;
+        transition-delay: 0s, 0s, 0.3s;
+      }
+    }
+  }
   ul li ul {
     opacity: 0;
     visibility: hidden;
@@ -14,33 +45,7 @@ export const NavBarWreper = styled.div`
 
     a {
       transition: all 0.3s ease 0s;
-      :hover {
-        color: #fa8d08;
-      }
     }
-  }
-
-  ul li {
-    float: left;
-    background: #494e54;
-    list-style: none;
-    color: white;
-    width: 120px;
-    height: 50px;
-    line-height: 50px;
-    text-align: center;
-    opacity: 1;
-    cursor: pointer;
-    :hover {
-      background-color: #6a6d7380;
-    }
-  }
-  .active {
-    visibility: visible;
-    opacity: 1;
-    z-index: 1;
-    transform: translateY(0%);
-    transition-delay: 0s, 0s, 0.3s;
   }
 `;
 
